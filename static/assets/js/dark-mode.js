9354cba3a2aa2c113da8559a9da1f963375919a0
chunsanger <chunsanger@live.com>
2025-03-17 23:24:27 -0400

    Fix dark mode toggle icon and improve text contrast

--- static/assets/js/dark-mode.js
+++ static/assets/js/dark-mode.js
@@ -27,7 +27,8 @@ document.addEventListener('DOMContentLoaded', function() {
     if (isDarkMode) {
       iconPath.setAttribute('d', 'M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z');
     } else {
-      iconPath.setAttribute('d', 'M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0m0 -9a9 9 0 0 0 9 9m-9 -15l0 3m-6.4 2.6l2.1 2.1m12.7 -2.1l-2.1 2.1m2.1 12.7l-2.1 -2.1m-12.7 2.1l2.1 -2.1m-3 -6.9l3 0');
+      // Simple sun icon
+      iconPath.setAttribute('d', 'M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0M12 3v2M12 19v2M19 12h2M3 12h2');
     }
   }
 }); 
